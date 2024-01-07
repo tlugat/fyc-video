d65358ce53ddd3da2cf49919f5eb18b5ce3873c1
unknown <echaimi@myges.fr>
2024-01-07 14:54:58 +0100

    last exo solar system

--- src/components/planet.jsx
+++ src/components/planet.jsx
@@ -1,7 +1,10 @@
-import React, { useRef, useMemo } from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+import { useRef, useState, useMemo } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Line } from "@react-three/drei";
 import * as THREE from "three";
+import { Html } from "@react-three/drei";
 
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
@@ -25,32 +28,62 @@ const OrbitPath = ({ distanceFromSun }) => {
   return <Line points={points} color="white" position={[0, 0, 0]} />;
 };
 
-const Planet = ({ texturePath, scale, orbitalSpeed, distanceFromSun }) => {
+const Planet = ({
+  name,
+  texturePath,
+  scale,
+  orbitalSpeed,
+  distanceFromSun,
+}) => {
   const planetRef = useRef();
   const orbitRef = useRef();
-  let theta = 0;
+  const thetaRef = useRef(0); // Utiliser useRef pour maintenir la valeur de theta
+  const [hovered, setHovered] = useState(false);
 
   const texture = useLoader(TextureLoader, texturePath);
 
   useFrame(() => {
-    if (planetRef.current) {
-      planetRef.current.rotation.y += 0.001; // Axial rotation
-    }
     if (orbitRef.current) {
-      theta += orbitalSpeed; // Orbital rotation
-      orbitRef.current.position.x = distanceFromSun * Math.cos(theta);
-      orbitRef.current.position.z = distanceFromSun * Math.sin(theta);
+      thetaRef.current += orbitalSpeed; // Mettre à jour thetaRef.current
+      orbitRef.current.position.x =
+        distanceFromSun * Math.cos(thetaRef.current);
+      orbitRef.current.position.z =
+        distanceFromSun * Math.sin(thetaRef.current);
     }
   });
+  const onPointerOver = () => {
+    setHovered(true);
+    console.log("Hovered over:", name); // Afficher le nom lors du survol
+  };
 
+  const onPointerOut = () => {
+    setHovered(false);
+    console.log("No longer hovered over:", name); // Afficher le nom lorsqu'on arrête de survoler
+  };
   return (
     <>
       <OrbitPath distanceFromSun={distanceFromSun} />
       <group ref={orbitRef}>
-        <mesh ref={planetRef} scale={scale}>
+        <mesh
+          ref={planetRef}
+          scale={
+            hovered ? [scale[0] * 1.2, scale[1] * 1.2, scale[2] * 1.2] : scale
+          }
+          onPointerOver={onPointerOver}
+          onPointerOut={onPointerOut}
+        >
           <sphereGeometry args={[1, 32, 32]} />
           <meshStandardMaterial map={texture} />
         </mesh>
+        {hovered && (
+          <Html position={[0, scale[0] * 1.5, 0]} transform occlude>
+            <div
+              style={{ color: "white", textAlign: "center", fontSize: "250px" }}
+            >
+              {name}
+            </div>
+          </Html>
+        )}
       </group>
     </>
   );
@@ -58,6 +91,7 @@ const Planet = ({ texturePath, scale, orbitalSpeed, distanceFromSun }) => {
 
 export const Sun = () => (
   <Planet
+    name="Sun"
     texturePath="/textures/sun.jpg"
     scale={[5, 5, 5]}
     orbitalSpeed={0}
@@ -66,6 +100,7 @@ export const Sun = () => (
 );
 export const Mercury = () => (
   <Planet
+    name="Mercury"
     texturePath="/textures/mercury.jpg"
     scale={[0.5, 0.5, 0.5]}
     orbitalSpeed={0.004}
@@ -74,6 +109,7 @@ export const Mercury = () => (
 );
 export const Venus = () => (
   <Planet
+    name="Venus"
     texturePath="/textures/venus.jpg"
     scale={[0.95, 0.95, 0.95]}
     orbitalSpeed={0.003}
@@ -82,6 +118,7 @@ export const Venus = () => (
 );
 export const Earth = () => (
   <Planet
+    name="Earth"
     texturePath="/textures/earth.jpg"
     scale={[1, 1, 1]}
     orbitalSpeed={0.002}
@@ -90,6 +127,7 @@ export const Earth = () => (
 );
 export const Mars = () => (
   <Planet
+    name="Mars"
     texturePath="/textures/mars.jpg"
     scale={[0.53, 0.53, 0.53]}
     orbitalSpeed={0.0018}
@@ -98,6 +136,7 @@ export const Mars = () => (
 );
 export const Jupiter = () => (
   <Planet
+    name="Jupiter"
     texturePath="/textures/jupiter.jpg"
     scale={[2.5, 2.5, 2.5]}
     orbitalSpeed={0.001}
@@ -106,6 +145,7 @@ export const Jupiter = () => (
 );
 export const Saturn = () => (
   <Planet
+    name="Saturn"
     texturePath="/textures/saturn.jpg"
     scale={[2.1, 2.1, 2.1]}
     orbitalSpeed={0.0009}
@@ -114,6 +154,7 @@ export const Saturn = () => (
 );
 export const Uranus = () => (
   <Planet
+    name="Uranus"
     texturePath="/textures/uranus.jpg"
     scale={[1.8, 1.8, 1.8]}
     orbitalSpeed={0.0004}
@@ -122,6 +163,7 @@ export const Uranus = () => (
 );
 export const Neptune = () => (
   <Planet
+    name="Neptune"
     texturePath="/textures/neptune.jpg"
     scale={[1.6, 1.6, 1.6]}
     orbitalSpeed={0.0003}
@@ -130,6 +172,7 @@ export const Neptune = () => (
 );
 export const Pluto = () => (
   <Planet
+    name="Pluto"
     texturePath="/textures/pluto.jpg"
     scale={[0.18, 0.18, 0.18]}
     orbitalSpeed={0.0002}
@@ -141,6 +184,9 @@ const moonOrbitalSpeed = 0.01;
 export const Moon = () => {
   const moonRef = useRef();
   let moonTheta = 0;
+
+  // Chargez la texture de la Lune en utilisant useLoader
+  const texture = useLoader(TextureLoader, "/textures/moon.jpg");
 
   useFrame(() => {
     if (moonRef.current) {
